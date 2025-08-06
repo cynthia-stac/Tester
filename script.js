6496019be74dd3fc93c5ef1e02d10cd5ff8a159b
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-08-06 12:30:16 +1100

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('main')
+const body = document.querySelector('body')
+const changeBackground = button.addEventListener(('click'), function(e){
+    body.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    console.log("Background color changed to: " + body.style.backgroundColor);
+}) 
+
+console.log("new background")
