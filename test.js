74b5c317b0a94bcb0b103821720aaf55cc308ecc
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-08-19 11:37:49 +1100

    Tested farther conditions of the for of loop

--- test.js
+++ test.js
@@ -5,11 +5,20 @@
 // }
 
 
-const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-for (const digit in digits) {
-  if (digit % 2 === 0) {
-    continue;
-  }
-  console.log(digit);
+// for (const digit in digits) {
+//   if (digit % 2 === 0) {
+//     continue;
+//   }
+//   console.log(digit);
+// }
+
+
+const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+
+
+for (const day of days){
+    var upperDay = day.charAt(0).toUpperCase() + day.slice(1)
+    console.log(upperDay)
 }
