cec54d394da13e31f643bc572e391352ba6ffa52
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-08-19 12:33:53 +1100

    Started with the arrow functions in ES6

--- test.js
+++ test.js
@@ -15,10 +15,15 @@
 // }
 
 
-const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+// const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 
 
-for (const day of days){
-    var upperDay = day.charAt(0).toUpperCase() + day.slice(1)
-    console.log(upperDay)
-}
+// for (const day of days){
+//     var upperDay = day.charAt(0).toUpperCase() + day.slice(1)
+//     console.log(upperDay)
+// }
+
+const upperizedNames = ['Farrin', 'Kagure', 'Asser'].map(
+  name => name.toUpperCase()
+);
+console.log(upperizedNames)
