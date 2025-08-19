6044fcdeb4e76ad65611cac2bfcf96c9096bd49d
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-08-19 11:11:03 +1100

    Tested farther conditions of the for of loop

--- test.js
+++ test.js
@@ -1,5 +1,15 @@
-const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-for (const index of digits) {
-  console.log(digits[index]);
+// for (const index of digits) {
+//   console.log(digits[index]);
+// }
+
+
+const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (const digit of digits) {
+  if (digit % 2 === 0) {
+    continue;
+  }
+  console.log(digit);
 }
