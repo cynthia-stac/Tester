23d80740e3fb7c54b14c1cd6dc4d9158f434e562
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-08-21 09:36:19 +1100

    Tested a number of aspects

--- test.js
+++ test.js
@@ -58,3 +58,5 @@
 function houseDescriptor([houseColor = 'green', shutterColors = ['red']]) {
   return `I have a ${houseColor} house with ${shutterColors.join(' and ')} shutters`;
 }
+
+console.log(houseDescriptor(['green']))
