de2833056cb8c1c592679cd6d85f4a2069766fd2
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-08-21 12:19:47 +1100

    Beginning with ES6 Classes

--- test.js
+++ test.js
@@ -70,3 +70,20 @@
 // console.log(buildHouse()); 
 // console.log(buildHouse({}))
 // console.log(buildHouse({floors: 3, color: 'yellow'}))
+
+class Dessert {
+  constructor(calories = 250) {
+    this.calories = calories;
+  }
+}
+
+class IceCream extends Dessert {
+  constructor(flavor, calories, toppings = []) {
+    super(calories);
+    this.flavor = flavor;
+    this.toppings = toppings;
+  }
+  addTopping(topping) {
+    this.toppings.push(topping);
+  }
+}
