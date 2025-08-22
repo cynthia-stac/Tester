92ae0f1f667c28ae931b63c16fa41c491e90f0b4
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-08-22 12:45:04 +1100

    Maps

--- test.js
+++ test.js
@@ -156,13 +156,33 @@
 // }
 
 
-let person = {name: "John"};
+// let person = {name: "John"};
 
-const weak = new Set();
-weak.add(person);
+// const weak = new Set();
+// weak.add(person);
 
-console.log(weak.has(person)); // true
+// console.log(weak.has(person)); // true
 
-person = null;
-console.log(person)
-console.log(weak)
+// person = null;
+// console.log(person)
+// console.log(weak)
+
+const employees = new Map();
+
+employees.set('james.parkes@udacity.com', { 
+    firstName: 'James',
+    lastName: 'Parkes',
+    role: 'Content Developer' 
+});
+employees.set('julia@udacity.com', {
+    firstName: 'Julia',
+    lastName: 'Van Cleve',
+    role: 'Content Developer'
+});
+employees.set('richard@udacity.com', {
+    firstName: 'Richard',
+    lastName: 'Kalehoff',
+    role: 'Content Developer'
+});
+
+console.log(employees);
