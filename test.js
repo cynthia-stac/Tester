b1e6726410766e78bf355accac3dc19aaec5a1d2
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-08-26 11:28:49 +1100

    New promise as an object

--- test.js
+++ test.js
@@ -240,12 +240,8 @@
 // const me = new Person('Sina')
 // // console.log(me.talk())
 
-new Promise(function () {
-    window.setTimeout(function createSundae(flavor = 'chocolate') {
-        const sundae = {};
-        // request ice cream
-        // get cone
-        // warm up ice cream scoop
-        // scoop generous portion into cone!
-    }, Math.random() * 2000);
+// 
+
+const myPromiseObj = new Promise(function (resolve, reject) {
+    // sundae creation code
 });
