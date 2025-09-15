330ec9bdb28e04411ed544bfafc8b79839cbec7f
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-09-15 21:08:15 +0200

    Done with the question

--- test.js
+++ test.js
@@ -707,3 +707,17 @@
 // }
 
 // console.log(flattenAndUnique())
+
+function compare(){
+  const array1 = [
+    { id: 1, name: "Alice", details: { age: 30, city: "New York" } },
+    { id: 2, name: "Bob", details: { age: 25, city: "Los Angeles" } }
+];
+
+const array2 = [
+    { id: 3, name: "Alice ", details: { age: 30, city: "New York" } },
+    { id: 2, name: "Bob", details: { age: 25, city: "Los Angeles" } }
+];
+  return JSON.stringify(array1) === JSON.stringify(array2)
+}
+console.log(compare())
