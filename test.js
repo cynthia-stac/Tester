18ae1e88e218f8f221183619aa6ef3838ef6006c
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-09-15 20:08:45 +0200

    Testing my program

--- test.js
+++ test.js
@@ -696,3 +696,14 @@
 
 // console.log(filterAndMap())
 
+// Write a function that takes a nested array of numbers and returns a new array that is flattened (one level deep) and contains only the unique numbers, in ascending order.
+// const nestedArray = [1, [2, 3], 4, [2, [5, 1]], 3];
+// const result = flattenAndUnique(nestedArray);
+// console.log(result); // Output: [1, 2, 3, 4, 5]
+
+function flattenAndUnique(){
+ const nestedArray = [1, [2, 3], 4, [2, [5, 1]], 3] 
+ return nestedArray.flat(Infinity).sort((a, b) => a - b)
+}
+
+console.log(flattenAndUnique())
