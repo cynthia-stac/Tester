bbbe3989a83d1b2a4009143d30ccb725a6fe7bfb
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-09-23 14:59:39 +0200

    Added other outputs

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello Theo, My Troubleshooter!");
+    System.out.println("Have a good day!");
+    System.out.println("Learning Java is fun!");
+
   }
 }
