acecaa5d5fade0bea751afb08e66a4e58dd3c0c7
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-09-23 15:23:40 +0200

    Learning the difference between println() and print()

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
   public static void main(String[] args) {
     System.out.print("Hello Theo, My Troubleshooter! ");
-    System.out.print("Have a good day! ");
+    System.out.println("Have a good day! ");
     System.out.print("Learning Java is fun!");
+    System.out.println("Try it out!");
 
   }
 }
