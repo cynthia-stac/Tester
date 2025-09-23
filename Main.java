894946f4e6240b2521328e4c4f2fefb37df75e8f
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-09-23 14:09:19 +0200

    Learning to update my class

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello Cynthia, My World!");
+    System.out.println("Hello Theo, My Troubleshooter!");
   }
 }
