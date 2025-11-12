647c4216e836a55de594cf018b0afe8f52e5a7fc
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-11-12 18:56:54 +0200

    The promise was a success

--- test.js
+++ test.js
@@ -824,6 +824,29 @@
 
 // console.log("After promise");
 
+const inventory = {
+  sunglasses: 1900,
+  pants: 1088,
+  bags: 1344
+};
+
+// Write your code below:
+const myExecutor = (resolve, reject) => {
+  if (inventory.sunglasses > 0) {
+    resolve('Sunglasses order processed.');
+  }
+   else {
+   reject('That item is sold out.');
+  }
+};
+const orderSunglasses = () => {
+  return new Promise(myExecutor);
+};
+
+const orderPromise = orderSunglasses();
+console.log(orderPromise)
+
+
 
 
  
