6e8af95771b739ccfd834ed533ef0e75538da83b
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-11-20 13:18:19 +0200

    Add the loadScript function

--- test.js
+++ test.js
@@ -874,6 +874,15 @@
 // prom.then(handleSuccess, handleFailure);
 
 
+function loadScript(src) {
+  // creates a <script> tag and append it to the page
+  // this causes the script with given src to start loading and run when complete
+  let script = document.createElement('script');
+  script.src = src;
+  document.head.append(script);
+}
+loadScript('script.js')
+
 
 
 
