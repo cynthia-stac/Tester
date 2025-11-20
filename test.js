a0a0fd1150d3f4b72483112b8228c27e6f71d9cc
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-11-20 13:43:39 +0200

    Analyze the invoking of a function in loadScript

--- test.js
+++ test.js
@@ -874,14 +874,17 @@
 // prom.then(handleSuccess, handleFailure);
 
 
-function loadScript(src) {
-  // creates a <script> tag and append it to the page
-  // this causes the script with given src to start loading and run when complete
+function loadScript(src, callback) {
   let script = document.createElement('script');
   script.src = src;
+  script.onload = () => callback(script);
   document.head.append(script);
 }
-loadScript('script.js')
+
+loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', script => {
+  alert(`Cool, the script ${script.src} is loaded`);
+  alert( _ ); // _ is a function declared in the loaded script
+});
 
 
 
