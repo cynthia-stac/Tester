e833c376d7017ca3f811929c3d63325fd50c239f
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-11-24 19:29:18 +0200

    Remove a semicolon

--- test.js
+++ test.js
@@ -878,13 +878,14 @@ function loadScript(src, callback) {
   let script = document.createElement('script');
   script.src = src;
   script.onload = () => callback(script);
-  document.head.append(script);
+  document.head.append('script')
 }
 
-loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', script => {
-  alert(`Cool, the script ${script.src} is loaded`);
-  alert( _ ); // _ is a function declared in the loaded script
-});
+let btn = document.getElementById('btn').addEventListener('click', loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', script))
+let script = () => {
+  console.log(`Cool, the script ${script.src} is loaded`);
+  console.log( _ ); // _ is a function declared in the loaded script
+};
 
 
 
