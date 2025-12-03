f5e481ed4cc255f1daec8f082167f3ed768d6ad4
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-12-03 16:31:03 +0200

    Introduce about data fetching

--- test.js
+++ test.js
@@ -907,26 +907,28 @@
 
 // promise.then(res => console.log(res));
 
-new Promise(function(resolve, reject) {
+// new Promise(function(resolve, reject) {
 
-  setTimeout(() => resolve(1), 1000); // (*)
+//   setTimeout(() => resolve(1), 1000); // (*)
 
-}).then(function(result) { // (**)
+// }).then(function(result) { // (**)
 
-  console.log(result); // 1
-  return result * 2;
+//   console.log(result); // 1
+//   return result * 2;
 
-}).then(function(result) { // (***)
+// }).then(function(result) { // (***)
 
-  console.log(result); // 2
-  return result * 2;
+//   console.log(result); // 2
+//   return result * 2;
 
-}).then(function(result) {
+// }).then(function(result) {
 
-  console.log(result); // 4
-  return result * 2;
+//   console.log(result); // 4
+//   return result * 2;
 
-});
+// });
+
+
   
 
 
