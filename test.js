66289c3e376ea04a09cb32be0ded521b3045e9cb
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-12-08 19:13:30 +0200

    Teach about how catch handles errors

--- test.js
+++ test.js
@@ -942,9 +942,26 @@
 //   });
 
   
+// fetch('https://no-such-server.blabla') // rejects
+//   .then(response => response.json())
+//   .catch(error => console.log(error))
 
 
+fetch('/article/promise-chaining/user.json')
+  .then(response => response.json())
+  .then(user => fetch(`https://api.github.com/users/${user.name}`))
+  .then(response => response.json())
+  .then(githubUser => new Promise((resolve, reject) => {
+    let img = document.createElement('img');
+    img.src = githubUser.avatar_url;
+    img.className = "promise-avatar-example";
+    document.body.append(img);
 
-
+    setTimeout(() => {
+      img.remove();
+      resolve(githubUser);
+    }, 3000);
+  }))
+  .catch(error => console.log(error.message));
  
 
