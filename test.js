df1cbf39e6efee157575ce82869cb18f4ad9299e
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-12-08 19:14:28 +0200

    Implicit try...catch

--- test.js
+++ test.js
@@ -947,21 +947,24 @@
 //   .catch(error => console.log(error))
 
 
-fetch('/article/promise-chaining/user.json')
-  .then(response => response.json())
-  .then(user => fetch(`https://api.github.com/users/${user.name}`))
-  .then(response => response.json())
-  .then(githubUser => new Promise((resolve, reject) => {
-    let img = document.createElement('img');
-    img.src = githubUser.avatar_url;
-    img.className = "promise-avatar-example";
-    document.body.append(img);
+// fetch('/article/promise-chaining/user.json')
+//   .then(response => response.json())
+//   .then(user => fetch(`https://api.github.com/users/${user.name}`))
+//   .then(response => response.json())
+//   .then(githubUser => new Promise((resolve, reject) => {
+//     let img = document.createElement('img');
+//     img.src = githubUser.avatar_url;
+//     img.className = "promise-avatar-example";
+//     document.body.append(img);
 
-    setTimeout(() => {
-      img.remove();
-      resolve(githubUser);
-    }, 3000);
-  }))
-  .catch(error => console.log(error.message));
+//     setTimeout(() => {
+//       img.remove();
+//       resolve(githubUser);
+//     }, 3000);
+//   }))
+//   .catch(error => console.log(error.message));
+
+
+
  
 
