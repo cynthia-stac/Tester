1fec242939b1815386efeb985d16ec7f3cd379f6
Cynthia Umubyeyi <umubnthia@gmail.com>
2025-12-16 19:02:59 +0200

    Map different fetched urls

--- test.js
+++ test.js
@@ -1015,8 +1015,23 @@
 // }).then(() => console.log("Next successful handler runs"));
 
 
-Promise.all([
-  new Promise(resolve => setTimeout(() => resolve(1), 3000)), // 1
-  new Promise(resolve => setTimeout(() => resolve(2), 2000)), // 2
-  new Promise(resolve => setTimeout(() => resolve(3), 1000))  // 3
-]).then((v) => console.log(v)); // 1,2,3 when promises are ready: each promise contributes an array member
+// Promise.all([
+//   new Promise(resolve => setTimeout(() => resolve(1), 1000)), // 1
+//   new Promise(resolve => setTimeout(() => resolve(2), 2000)), // 2
+//   new Promise(resolve => setTimeout(() => resolve(3), 3000))  // 3
+// ]).then((v) => console.log(v)); // 1,2,3 when promises are ready: each promise contributes an array member
+
+let urls = [
+  'https://api.github.com/users/iliakan',
+  'https://api.github.com/users/remy',
+  'https://api.github.com/users/jeresig'
+];
+
+// map every url to the promise of the fetch
+let requests = urls.map(url => fetch(url));
+
+// Promise.all waits until all jobs are resolved
+Promise.all(requests)
+  .then(responses => responses.forEach(
+    response => alert(`${response.url}: ${response.status}`)
+  ));
