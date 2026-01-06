4753c3cbbce87b55d4f2271bc5393252cc852bdb
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-01-06 19:02:09 +0200

    Work on a new pizza order

--- test.ts
+++ test.ts
@@ -1,14 +1,28 @@
 const menu = [
-    {name: "Margarita", price: 8},
-    {name: "Peperroni", price: 8},10
-    {name: "Hawaiian", price: 810},
-    {name: "Veggie", price:98},
+    { name: "Margherita", price: 8 },
+    { name: "Pepperoni", price: 10 },
+    { name: "Hawaiian", price: 10 },
+    { name: "Veggie", price: 9 },
 ]
 
 const cashInRegister = 100
+const nextOrderId = 1
 const orderQueue = []
 
+function addNewPizza(pizzaObj) {
+    menu.push(pizzaObj)
+}
 
-function addNewPizza(pizza) { 
-   menu.push(pizza)
+function placeOrder(pizzaName) {
+    const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
+    cashInRegister += selectedPizza.price
+    const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
+    orderQueue.push(newOrder)
+    return newOrder 
+}
+
+function completeOrder(orderId) {
+    const order = orderQueue.find(order => order.id === orderId)
+    order.status = "completed"
+    return order
 }
