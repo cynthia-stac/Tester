610ece0ed5d7f45526c72c879afb6d16d64ac162
Cynthia Umubyeyi <umubnthia@gmail.com>
2026-01-06 18:57:30 +0200

    Introduced to typescript

---
+++ test.ts
@@ -0,0 +1,14 @@
+const menu = [
+    {name: "Margarita", price: 8},
+    {name: "Peperroni", price: 8},10
+    {name: "Hawaiian", price: 810},
+    {name: "Veggie", price:98},
+]
+
+const cashInRegister = 100
+const orderQueue = []
+
+
+function addNewPizza(pizza) { 
+   menu.push(pizza)
+}
